4e4c4e0984b7f06d39b712319a993838efc99d38
sjnp <supawet.jnp@gmail.com>
2022-01-07 22:05:31 +0700

    Fix controller path in app.js

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use('/api/positions', controller.positions)
 app.use('/api/items', controller.items)
 app.use('/api/trackingHistories', controller.trackingHistories)
-app.use('/api/staffs', controller.trackingHistories)
+app.use('/api/staffs', controller.staffs)
 app.use('/api/itemStates', controller.itemStates)
 app.use('/api/branches', controller.branches)
 
