546a7e9866e4a27fc5543a44fcc942511a91a643
sjnp <supawet.jnp@gmail.com>
2022-01-07 20:36:02 +0700

    Add home page and route to login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch, Route, Link
+} from 'react-router-dom'
 import './App.css';
-import AddStaffForm from './components/AddStaffForm';
-import Login from './components/Login'
+import Home from "./pages/Home"
+import LoginPage from './pages/Login';
 
 const App = () => {
 
   return (
-    <React.Fragment>
+    <Router>
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css"></link>
-      <Login />
-      <AddStaffForm />
-    </React.Fragment>
+      <Switch>
+        <Route exact path='/' >
+          <Home />
+        </Route>
+        <Route path='/login'>
+          <LoginPage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchParcel from "../components/SearchParcel";
+
+const HomePage = () => {
+  return (
+    <div>
+      <SearchParcel />
+    </div>
+  )
+}
+
+export default HomePage
