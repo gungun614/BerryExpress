483b3586940ccaa8c2c16c2579ff76869072e52c
gungun614 <6105003484@rumail.ru.ac.th>
2022-01-08 21:56:00 +0700

    addConsigneeDetail

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
+import Label from "./widgets/Label"
 import Login from './components/Login'
+import  AddBranch from './components/AddBranch'
+import ConsigneeDetail from './components/ConsigneeDetali'
 
 const App = () => {
 
   return (
     <React.Fragment>
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css"></link>
-      <Login />
+      {/* <Login /> */}
+      {/* <Label text="เพิ่มข้อมูลสาขา" /> */}
+      {/* <Label text="ยืนยันการบันทึกข้อมูล" /> */}
+      {/* <AddBranch /> */}
+      <ConsigneeDetail/>
     </React.Fragment>
   );
 }
