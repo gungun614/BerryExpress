7abd9931982f3c723cd969cbce04f0575c48beef
gungun614 <6105003484@rumail.ru.ac.th>
2022-01-09 18:06:13 +0700

    edit page addbranch

--- frontend/src/pages/AddBranch.js
+++ frontend/src/pages/AddBranch.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import Label from "../widgets/Label"
 import Input from "../widgets/Input"
+import Button from "../widgets/Button"
 
 const AddBranch = () => {
   
@@ -14,21 +15,37 @@ const AddBranch = () => {
     const handleChangeTelBranch = (event) => setTelBranch(event.target.value)
     const handleChangeStartDate= (event) => setStartDate(event.target.value)
 
-
+    const [ isTitle, setIsTitle ] = useState(false)
+    const [ isButton, setIsButton ] = useState(false)
+    const handleClickToggle = () => {
+      setIsTitle(!isTitle)
+      setIsButton(!isButton)
+      setIsEdit(!isEdit)
+    }
+    const [ isEdit, setIsEdit ] = useState(false)
 
   return (
     <div>
+
+  <Label text="เพิ่มข้อมูลสาขา" isHide = {isTitle} />
+  <Label text="ยืนยันการบันทึกข้อมูล" isHide = {!isTitle} />
+  <br/>
     <Label text="ชื่อสาขา" />
-    <Input type = "text" value = {branchName} onChange = {handleChangeBranchName} /> 
+    <Input type = "text" value = {branchName} onChange = {handleChangeBranchName} disabled ={isEdit} /> 
     <br/>
     <Label text="ที่อย่สาขา" />
-    <Input type = "text" value = {addressBranch} onChange = {handleChangeAddressBranch} />
+    <Input type = "text" value = {addressBranch} onChange = {handleChangeAddressBranch} disabled ={isEdit} />
     <br/>
     <Label text="เบอร์โทร"  />
-    <Input type = "text" value = {telBranch} onChange = {handleChangeTelBranch} />
+    <Input type = "text" value = {telBranch} onChange = {handleChangeTelBranch} disabled ={isEdit} />
     <br/>
     <Label text="วันที่ก่อตั้ง" />
-    <Input type = "text" value = {startDate} onChange = {handleChangeStartDate} />
+    <Input type = "text" value = {startDate} onChange = {handleChangeStartDate} disabled ={isEdit} />
+    <br/>
+    <Button type={'button'} text={'บันทึก'} isHide = {isButton} onClick = {handleClickToggle}  />
+    <Button type={'button'} text={'แก้ไข'} isHide = {!isButton} onClick = {handleClickToggle} />
+    <Button type={'button'} text={'ยืนยันการบันทึก'} isHide = {!isButton}/>
+     
  </div>
       
   )
