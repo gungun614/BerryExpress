049b4e0dbdd85f37f69fbab6d37a0c1016dbbfd2
MitkuSA <mit_sanbua@hotmail.com>
2022-01-09 19:59:06 +0700

    Add HeaderBar Component V2

--- frontend/src/components/HeaderBar.js
+++ frontend/src/components/HeaderBar.js
@@ -1,12 +1,12 @@
 import Label from "../widgets/Label"
-import Icon from "../widgets/Icon"
+import logo from "../image/cherryLogo.svg"
 
 const HeaderBar = () => {
 
     return(
         <div>
             <div style={{ backgroundColor : "#E0E0E0" , width:"100%" , height :"60px" , display: "flex" ,  alignItems: "center"}}>
-                <Icon className="bi bi-apple" size="40px" />
+                <img src={logo} alt="Logo" width="40px"/>
                 <Label text ="Berry Express" size="20px" />
                 <hr/>
             </div>
